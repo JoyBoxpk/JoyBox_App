ce8e6b6b942c2a518dc81b52dca513690c52488e
hafeezullah99 <hafeez.ullah@joybox.pk>
2024-03-25 15:58:08 -0700

    Project

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
                 ],
                 supportedLocales: [
                   Locale(
-                    'en',
+                    'ena',
                     '',
                   ),
                 ],
